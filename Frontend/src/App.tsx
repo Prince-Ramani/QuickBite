0e9efacb7803aa0800d3cdce1b3336b98d83bebd
Prince-Ramani <princeramani67@gmail.com>
2025-06-26 09:03:07 +0530

    init..

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import "./index.css";
+import { Button } from "./components/ui/button";
+function App() {
+  return (
+    <div className="text-blue-600">
+      Hello world
+      <Button>Hwlloe </Button>
+    </div>
+  );
+}
+
+export default App;
